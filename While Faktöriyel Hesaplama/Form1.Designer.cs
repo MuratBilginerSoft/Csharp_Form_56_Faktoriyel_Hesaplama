a8aa4ab4fc5be98adacb2412d3f28d277a914e56
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:45:25 +0300

    Initial commit

---
+++ While Faktöriyel Hesaplama/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace While_Faktöriyel_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TextDeger = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnFaktöriyel = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TextSonuc = new System.Windows.Forms.TextBox();
+            this.LblSonuc = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TextDeger);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 76);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(233, 79);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi";
+            // 
+            // TextDeger
+            // 
+            this.TextDeger.Location = new System.Drawing.Point(98, 35);
+            this.TextDeger.Name = "TextDeger";
+            this.TextDeger.Size = new System.Drawing.Size(128, 21);
+            this.TextDeger.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N Değeri Gir";
+            // 
+            // BtnFaktöriyel
+            // 
+            this.BtnFaktöriyel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFaktöriyel.Location = new System.Drawing.Point(16, 162);
+            this.BtnFaktöriyel.Name = "BtnFaktöriyel";
+            this.BtnFaktöriyel.Size = new System.Drawing.Size(233, 29);
+            this.BtnFaktöriyel.TabIndex = 1;
+            this.BtnFaktöriyel.Text = "Faktöriyel Hesapla";
+            this.BtnFaktöriyel.UseVisualStyleBackColor = true;
+            this.BtnFaktöriyel.Click += new System.EventHandler(this.BtnFaktöriyel_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TextSonuc);
+            this.groupBox2.Controls.Add(this.LblSonuc);
+            this.groupBox2.Location = new System.Drawing.Point(16, 197);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(233, 77);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sonuç";
+            // 
+            // TextSonuc
+            // 
+            this.TextSonuc.Location = new System.Drawing.Point(97, 37);
+            this.TextSonuc.Name = "TextSonuc";
+            this.TextSonuc.Size = new System.Drawing.Size(128, 21);
+            this.TextSonuc.TabIndex = 3;
+            // 
+            // LblSonuc
+            // 
+            this.LblSonuc.AutoSize = true;
+            this.LblSonuc.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSonuc.Location = new System.Drawing.Point(44, 39);
+            this.LblSonuc.Name = "LblSonuc";
+            this.LblSonuc.Size = new System.Drawing.Size(47, 17);
+            this.LblSonuc.TabIndex = 2;
+            this.LblSonuc.Text = "Sonuç";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 309);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 41);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(55, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "www.industryolog.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(55, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Industryolog Akademi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::While_Faktöriyel_Hesaplama.Properties.Resources._01___Logo_2017;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(265, 350);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.BtnFaktöriyel);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Faktöriyel Hesabı";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TextDeger;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnFaktöriyel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TextSonuc;
+        private System.Windows.Forms.Label LblSonuc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ While Faktöriyel Hesaplama/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While_Faktöriyel_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFaktöriyel_Click(object sender, EventArgs e)
+        {
+            int factorial = 1, n = 0, i = 1;
+
+            n = Convert.ToInt32(TextDeger.Text);
+
+            if (n == 0 || n == 1)
+            {
+                factorial = 1;
+            }
+
+            else
+            {
+                while (i <= n)
+                {
+                    factorial *= i;
+                    i++;
+                }
+            }
+
+            LblSonuc.Text = n + "!=";
+            TextSonuc.Text = factorial.ToString();
+        }
+    }
+}
